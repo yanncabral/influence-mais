1743fe4408a71e12816d009a27d03d765fa2b7b8
Yann <iamyanndias@gmail.com>
2020-07-22 15:21:29 -0300

    initial release - xcode structure

---
+++ Influence+/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Influence+
+//
+//  Created by Yann Cabral on 22/07/20.
+//  Copyright © 2020 Yann Cabral. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
